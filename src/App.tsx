ab006b73fa0fb829e86faf643929213a82c43ee7
OverTheMil <github@overthemil.com>
2023-09-01 22:41:40 +1000

    Add shadcnui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-function App() {
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
+function App() {
   return (
     <>
-      <h1> Ticket </h1>
+      <Card className="bg-blue-50">
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
     </>
   )
 }
